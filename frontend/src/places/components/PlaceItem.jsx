9472a7ca6f4c50a09d6d8fe4a0ff5544651a8485
Sergey Kudashev <sergey.m.kudashev@gmail.com>
2025-05-26 16:50:06 +0300

    add SideDrop for Map

--- frontend/src/places/components/PlaceItem.jsx
+++ frontend/src/places/components/PlaceItem.jsx
@@ -1,27 +1,41 @@
+import { useState } from "react";
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
 import "./PlaceItem.css"
+import Modal from "../../shared/components/UIElements/Modal";
+
 
 const PlaceItem = props => {
+
+    const [showMap, setShowMap] = useState(false);
+
+    const openMapHandler = () => setShowMap(true)
+
+    const closeMapHandler = () => setShowMap(false)
     return (
-        <Card className="place-item__content">
-            <li className="place-item">
-                <div className="place-item__image">
-                    <img src={props.image}/>
-                </div>
-                <div className="place-item__info">
-                    <h2>{props.title}</h2>
-                    <address>{props.address}</address>
-                    <p>{props.description}</p>
-                </div>
-                <div className="place-item__actions">
-                    <Button inverse>Посмотреть на карте</Button>
-                    <Button to={`/places/${props.id}`}>Изменить</Button>
-                    <Button danger>Удалить</Button>
-                </div>
-            </li>
-        </Card>
+
+        <>
+        <Modal show={showMap} onCancel={closeMapHandler}/>
+
+            <Card className="place-item__content">
+                <li className="place-item">
+                    <div className="place-item__image">
+                        <img src={props.image} />
+                    </div>
+                    <div className="place-item__info">
+                        <h2>{props.title}</h2>
+                        <address>{props.address}</address>
+                        <p>{props.description}</p>
+                    </div>
+                    <div className="place-item__actions">
+                        <Button inverse onClick={openMapHandler}>Посмотреть на карте</Button>
+                        <Button to={`/places/${props.id}`}>Изменить</Button>
+                        <Button danger>Удалить</Button>
+                    </div>
+                </li>
+            </Card>
+        </>
     );
 }
- 
+
 export default PlaceItem;
